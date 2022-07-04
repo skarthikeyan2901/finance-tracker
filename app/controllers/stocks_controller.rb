fbaaf0c4492f35d62dc130dc70847a44807a9303
Karthikeyan <skarthikeyan2901@gmail.com>
2022-07-04 12:22:52 +0530

    ajax form submission added

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,10 +1,14 @@
 class StocksController < ApplicationController
+  protect_from_forgery except: :search
 
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-        render 'users/my_portfolio'
+        respond_to do |format|
+          format.js { render partial: 'stocks/results' }
+        end
+        # render 'users/my_portfolio'
       else
         flash[:alert] = "Please enter a valid symbol to search"
         redirect_to my_portfolio_path
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+jQuery("#form-submit-button").on("click", function(event) {
+  // event.preventDefault();
+  var formButtonElement = jQuery("#form-submit-button");
+  var formInput = jQuery("#stock-input");
+  var stock = formInput.val();
+  jQuery.ajax({
+    url: formButtonElement.data("url"),
+    data: { stock: stock },
+    method: 'GET',
+    success: function(result) {
+      console.log(result)
+    }
+  })
+})
